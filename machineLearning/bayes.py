df17b99a4e928b43f465a865ce67f33aea4d91ad
a946153815 <lhx_sgg@163.com>
2018-08-30 20:30:45 +0800

    lhx

--- machineLearning/bayes.py
+++ machineLearning/bayes.py
@@ -1,4 +1,9 @@
+import operator
+from math import log
+
 import numpy as np
+
+
 def loadDataSet():
     postingList=[['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d']]
     classVec=[0,1,0,1,0,1]
@@ -21,7 +26,7 @@ def trainNB0(trainMatrix,trainCategory):
     numTrainDocs=len(trainMatrix)
     numWords=len(trainMatrix[0])
     pAbusive=sum(trainCategory)/float(numTrainDocs)
-    p0Num=np.zeros(numWords);p1Num=np.zeros(numWords)
+    p0Num=np.ones(numWords);p1Num=np.ones(numWords)
     p0Denom=0.0;p1Denom=0.0
     for i in range(numTrainDocs):
         if trainCategory[i]==1:
@@ -32,6 +37,16 @@ def trainNB0(trainMatrix,trainCategory):
             p0Denom+=sum(trainMatrix[i])
             pass
         pass
-    p1Vect=p1Num/p1Denom
-    p0Vect=p0Num/p0Denom
+    p1Vect=log(p1Num/p1Denom)
+    p0Vect=log(p0Num/p0Denom)
     return p0Vect,p1Vect,pAbusive
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1=sum(vec2Classify*p1Vec)+log(pClass1)
+    p0=sum(vec2Classify*p1Vec)+log(1.0-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+def testingNB(parameter_list):
+    listOPosts,listClasses=loadDataSet()
+    pass

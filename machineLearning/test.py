9ca6fa831f6d3cc5cd6a0def4e69a41390da8de3
a946153815 <lhx_sgg@163.com>
2018-08-31 10:32:55 +0800

    lhx

--- machineLearning/test.py
+++ machineLearning/test.py
@@ -1,15 +1,12 @@
-import cvHelper
 import numpy as np
+
 import bayes
-import numpy as np
+import cvHelper
 
 #cvHelper.lhx_CameraPreview()
 dataset,de=bayes.loadDataSet()
-<<<<<<< HEAD
 print(dataset)
 group=np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
 print(group.transpose(1,0))
-=======
-r=bayes.createVocabList(dataset)
+bayes.createVocabList(dataset)
 bayes.testingNB()
->>>>>>> e88870b72a5b325083320bc28c74d7d6c5437a62

19f9f27da9b0516e34ed47f4217c9291a36eab12
a946153815 <lhx_sgg@163.com>
2018-08-31 10:21:03 +0800

    lhx

--- machineLearning/test.py
+++ machineLearning/test.py
@@ -1,5 +1,9 @@
 import cvHelper
 import bayes
+import numpy as np
+
 #cvHelper.lhx_CameraPreview()
 dataset,de=bayes.loadDataSet()
 print(dataset)
+group=np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+print(group.transpose(1,0))

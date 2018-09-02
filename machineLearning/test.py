bbd7e8db123449df8a92c3dc18703da8666d43da
a946153815 <lhg_sgg@163.com>
2018-09-02 21:42:18 +0800

    lhx

--- machineLearning/test.py
+++ machineLearning/test.py
@@ -1,7 +1,13 @@
-import cvHelper
 import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
+
 import bayes
+import cvHelper
+
 #cvHelper.lhx_CameraPreview()
-dataset,de=bayes.loadDataSet()
-r=bayes.createVocabList(dataset)
-bayes.testingNB()
+#dataset,de=bayes.loadDataSet()
+#r=bayes.createVocabList(dataset)
+#bayes.testingNB()
+arr=Series([1,2],index=['dd','db'])
+print(arr)

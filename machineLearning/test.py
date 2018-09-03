6a760e283a521dc026ec86a8f37e8f7cd230843a
a946153815 <lhx_sgg@163.com>
2018-09-03 15:29:32 +0800

    lhx

--- machineLearning/test.py
+++ machineLearning/test.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from math import log
 import bayes
 import cvHelper
 
@@ -10,3 +10,4 @@ group=np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
 print(group.transpose(1,0))
 bayes.createVocabList(dataset)
 bayes.testingNB()
+print(log(log(5)))

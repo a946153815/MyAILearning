7824313f056b8c34119aafb8575081e98570b5a2
a946153815 <lhx_sgg@163.com>
2018-09-13 20:56:34 +0800

    lhx

--- machineLearning/logRegres.py
+++ machineLearning/logRegres.py
@@ -26,3 +26,37 @@ def gradAcsent(dataMatIn,classLabels):
         weights=weights+alpha*dataMatrix.transpose()*error
         pass
     return weights
+#随机梯度上升算法
+def stocGradAscent0(dataMatrix,classLabels):
+    m,n=np.shape(dataMatrix)
+    alpha=0.01
+    weights=np.ones(n)
+    for i in range(m):
+        h=sigmoid(sum(dataMatrix[i]*weights))
+        error=classLabels[i]-h
+        weights=weights+alpha*error*dataMatrix[i]
+        pass
+    return weights
+def plotBestFit(wei):
+    import matplotlib.pyplot as plt
+    weights=wei.getA()
+    dataMat,labelMat=loadDataSet()
+    dataArr=np.array(dataMat)
+    n=np.shape(dataArr)[0]
+    xcord1=[];ycord1=[]
+    xcord2=[];ycord2=[]
+    for i in range(n):
+        if int(labelMat[i])==1:
+            xcord1.append(dataArr[i,1]);ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]);ycord2.append(dataArr[i,2])
+        pass
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+    x=np.arange(-3.0,3.0,0.1)
+    ax.plot(x,y)
+    plt.xlabel('X1');plt.ylabel('x2');
+    plt.show
+    pass

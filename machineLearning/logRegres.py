e468ca0630a6538891d787b5aac986cb3812f59a
a946153815 <lhx_sgg@163.com>
2018-09-13 20:32:12 +0800

    lhx

--- machineLearning/logRegres.py
+++ machineLearning/logRegres.py
@@ -11,3 +11,18 @@ def loadDataSet():
     return dataMat,labelMat
 def sigmoid(inX):
     return 1.0/(1+np.exp(-inX))
+
+#Logistic回归梯度上升算法
+def gradAcsent(dataMatIn,classLabels):
+    dataMatrix=np.mat(dataMatIn)
+    labelMat=np.mat(classLabels).transpose()
+    m,n=np.shape(dataMatrix)
+    alpha=0.001
+    maxCycles=500
+    weights=np.ones((n,1))
+    for k in range(maxCycles):
+        h=sigmoid(dataMatrix*weights)
+        error=(labelMat-h)
+        weights=weights+alpha*dataMatrix.transpose()*error
+        pass
+    return weights

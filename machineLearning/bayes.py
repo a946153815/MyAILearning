fb21e709b0fe4c65a7e9351a086659b5ca482c99
a946153815 <lhx_sgg@163.com>
2018-09-26 08:52:50 -0700

    lhx

---
+++ machineLearning/bayes.py
@@ -0,0 +1,64 @@
+import operator
+from math import log
+
+import numpy as np
+
+
+def loadDataSet():
+    postingList=[['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d'],['m','y','d','d','d','d','d']]
+    classVec=[0,1,0,1,0,1]
+    return postingList,classVec
+
+def createVocabList(dataSet):
+    vocabSet=set([])
+    for document in dataSet:
+        vocabSet=vocabSet|set(document)
+    return list(vocabSet)
+def setOfWords2Vec(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]=1
+        else:print("the word :%s is not in my Vacabulary!"%word)
+    return returnVec
+#贝叶斯训练
+def trainNB0(trainMatrix,trainCategory):
+    numTrainDocs=len(trainMatrix)
+    numWords=len(trainMatrix[0])
+    pAbusive=sum(trainCategory)/float(numTrainDocs)
+    p0Num=np.ones(numWords);p1Num=np.ones(numWords)
+    p0Denom=2.0;p1Denom=2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i]==1:
+            p1Num+=trainMatrix[i]
+            p1Denom+=sum(trainMatrix[i])
+            print(trainMatrix[i])
+            print(p1Denom)
+        else:
+            p0Num+=trainMatrix[i]
+            p0Denom+=sum(trainMatrix[i])
+            pass
+        pass
+    p1Vect=np.log(p1Num/p1Denom)
+    p0Vect=np.log(p0Num/p0Denom)
+    return p0Vect,p1Vect,pAbusive
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1=sum(vec2Classify*p1Vec)+log(pClass1)
+    p0=sum(vec2Classify*p1Vec)+log(1.0-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+def testingNB():
+    listOPosts,listClasses=loadDataSet()
+    myVocabList=createVocabList(listOPosts)
+    trainMat=[]
+    for postinDoc in listOPosts:
+        print(postinDoc)
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    p0V,p1V,pAb=trainNB0(np.array(trainMat),np.array(listClasses))
+    testEntry=['love','my','dalmation']
+    thisDoc=np.array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,'classified as:',classifyNB(thisDoc,p0V,p1V,pAb))
+    pass
+pass
---
+++ machineLearning/test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
+
+import bayes
+import cvHelper
+import logRegres
+
+#cvHelper.lhx_CameraPreview()
+#dataset,de=bayes.loadDataSet()
+#r=bayes.createVocabList(dataset)
+
+#bayes.testingNB()0
+#arr=Series([1,2],index=['dd','db'])
+#print(arr)
+arr=np.array([[4,1,0],[4,2,0],[4,3,0],[4,1,0],[4,1,0]])
+label=np.array([1,1,0,0,1])
+label=np.mat(label).transpose()
+m=np.mat(arr)
+w=np.ones((3,1))
+er=label-logRegres.sigmoid(m*w)
+print(arr)
+print("\n")
+print(w)
+print("\n")
+print(m.transpose())
+print("\n")
+print(er)
+print("\n")
+print(0.001*m.transpose()*er)
